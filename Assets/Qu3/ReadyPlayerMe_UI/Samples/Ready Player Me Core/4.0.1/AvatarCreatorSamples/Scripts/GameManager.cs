90985fc9cbc4f04b56f79c69452f317fcd762009
Atul <125960822+AtulAbhiwan03@users.noreply.github.com>
2023-12-12 18:59:59 +0530

    Male Animations added

---
+++ Assets/Qu3/ReadyPlayerMe_UI/Samples/Ready Player Me Core/4.0.1/AvatarCreatorSamples/Scripts/GameManager.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using ReadyPlayerMe;
+using ReadyPlayerMe.AvatarCreator;
+using ReadyPlayerMe.Core;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+    {
+        [SerializeField] private AvatarCreatorStateMachine avatarCreatorStateMachine;
+        [SerializeField] private AvatarConfig inGameConfig;
+        private AvatarObjectLoader avatarObjectLoader;
+        public GameObject LoadedAvatar;
+        public GameObject FemaleAnimationPanel;
+        public GameObject MaleAnimationPanel;
+        [SerializeField] private Transform FemaleAnimationContent;
+        [SerializeField] private Transform MaleAnimationContent;
+        [SerializeField] internal List<RuntimeAnimatorController> FemaleAnimationList = new List<RuntimeAnimatorController>();
+        [SerializeField] internal List<RuntimeAnimatorController> MaleAnimationList = new List<RuntimeAnimatorController>();
+        private void OnEnable()
+        {
+            avatarCreatorStateMachine.AvatarSaved += OnAvatarSaved;
+        }
+
+        private void OnDisable()
+        {
+            avatarCreatorStateMachine.AvatarSaved -= OnAvatarSaved;
+            avatarObjectLoader?.Cancel();
+        }
+
+        private void Start()
+        {
+            ChangeFemaleAnimation();
+            ChangeMaleAnimation();
+        }
+
+        private void OnAvatarSaved(string avatarId)
+        {
+            Debug.Log(gameObject.name + " " + avatarId);
+            
+            
+            avatarCreatorStateMachine.gameObject.SetActive(false);
+
+            var startTime = Time.time;
+            avatarObjectLoader = new AvatarObjectLoader();
+            avatarObjectLoader.AvatarConfig = inGameConfig;
+            avatarObjectLoader.OnCompleted += (sender, args) =>
+            {
+                AvatarAnimatorHelper.SetupAnimator(args.Metadata.BodyType, args.Avatar);
+                LoadedAvatar = args.Avatar;
+                if (args.Metadata.OutfitGender == OutfitGender.Feminine)
+                {
+                    FemaleAnimationPanel.SetActive(true);
+                }
+                else if (args.Metadata.OutfitGender == OutfitGender.Masculine)
+                {
+                    MaleAnimationPanel.SetActive(true);
+                }
+            };
+            avatarObjectLoader.LoadAvatar(AvatarEndpoints.GetAvatarPublicUrl(avatarId));
+        }
+        
+        
+        internal void ChangeFemaleAnimation()
+        {
+            int i = 0;
+            foreach (Transform child in FemaleAnimationContent)
+            {
+                int temp = i;
+                child.GetComponent<Button>().onClick.AddListener(() =>
+                    {
+                        LoadedAvatar.GetComponent<Animator>().runtimeAnimatorController = FemaleAnimationList[temp];
+                    }
+                );
+                i++;
+            }
+            
+            
+        } 
+        
+        internal void ChangeMaleAnimation()
+        {
+            int i = 0;
+            foreach (Transform child in MaleAnimationContent)
+            {
+                int temp = i;
+                child.GetComponent<Button>().onClick.AddListener(() =>
+                    {
+                        LoadedAvatar.GetComponent<Animator>().runtimeAnimatorController = MaleAnimationList[temp];
+                    }
+                );
+                i++;
+            }
+            
+            
+        }
+    }
+
---
+++ Assets/Qu3/ReadyPlayerMe_UI/Samples/Ready Player Me Core/4.0.1/AvatarLoadingSamples/AvatarLoading/AvatarLoadingExample.cs
@@ -0,0 +1,52 @@
+﻿using ReadyPlayerMe.Core;
+using UnityEngine;
+
+namespace ReadyPlayerMe.Samples
+{
+    /// <summary>
+    /// This class is a simple <see cref="Monobehaviour"/>  to serve as an example on how to load Ready Player Me avatars and spawn as a <see cref="GameObject"/> into the scene.
+    /// </summary>
+    public class AvatarLoadingExample : MonoBehaviour
+    {
+        [SerializeField, Tooltip("Set this to the URL or shortcode of the Ready Player Me Avatar you want to load.")]
+        private string avatarUrl = "https://api.readyplayer.me/v1/avatars/638df693d72bffc6fa17943c.glb";
+
+        private GameObject avatar;
+        // Add 2 editable fields to store the new masculine and feminine animator controllers
+        [SerializeField] private RuntimeAnimatorController masculineController;
+        [SerializeField] private RuntimeAnimatorController feminineController;
+
+        private void Start()
+        {
+            ApplicationData.Log();
+            var avatarLoader = new AvatarObjectLoader();
+            // use the OnCompleted event to set the avatar and setup animator
+            avatarLoader.OnCompleted += (_, args) =>
+            {
+                avatar = args.Avatar;
+                SetAnimatorController(args.Metadata.OutfitGender); //  <--------------- ADDED
+            };
+            avatarLoader.LoadAvatar(avatarUrl);
+        }
+		
+        // This method is used to reassign the appropriate animator controller based on outfit gender
+        private void SetAnimatorController(OutfitGender outfitGender)
+        {
+            var animator = avatar.GetComponent<Animator>();
+            // set the correct animator based on outfit gender
+            if (animator != null && outfitGender == OutfitGender.Masculine)
+            {
+                animator.runtimeAnimatorController = masculineController;
+            }
+            else
+            {
+                animator.runtimeAnimatorController = feminineController;
+            }
+        }
+
+        private void OnDestroy()
+        {
+            if (avatar != null) Destroy(avatar);
+        }
+    }
+}
